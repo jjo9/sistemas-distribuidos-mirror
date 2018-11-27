da935c9a7049a25896a45f45b429364be400a401
Jose <malun224@gmail.com>
2018-11-27 23:19:19 +0000

    Begin
    Inicio do projeto

---
+++ UberSimples/src/ubersimples/Cliente.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ubersimples;
+
+/**
+ *
+ * @author Crisanto
+ */
+public class Cliente {
+    
+    // Cliente User
+    
+    // Registar;
+    // Logar;
+    // Inserir origem da viagem;
+    // Inserir destino da viagem;
+    // Solicitar uma viatura com condutor para uma viagem específica;
+    // Atribuir uma pontuação (1 a 5) ao condutor para uma viagem específica;
+    // Visualizar o seu histórico de viagens e respetiva pontuação atribuída;
+    // Sair.
+    
+    // Cliente Condutor
+    
+    // Registar;
+    // Logar;
+    // Alterar estado (disponível / indisponível)
+    // Receber pedidos de viatura com condutor para uma viagem específica e aceitar ou rejeitar os mesmos;
+    // Visualizar o seu histórico de viagens e respetiva pontuação recebida;
+    // Sair.
+
+    // o que têm em comum
+
+}
---
+++ UberSimples/src/ubersimples/ClienteCondutor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ubersimples;
+
+/**
+ *
+ * @author Crisanto
+ */
+public class ClienteCondutor {
+    
+    // Cliente Consumidor Final
+    
+    // Registar;
+    // Logar;
+    // Alterar estado (disponível / indisponível)
+    // Receber pedidos de viatura com condutor para uma viagem específica e aceitar ou rejeitar os mesmos;
+    // Visualizar o seu histórico de viagens e respetiva pontuação recebida;
+    // Sair.
+    
+}
