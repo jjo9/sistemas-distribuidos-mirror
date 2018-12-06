f741f13d3d37f2e520742eafb65dfb56b0c104ae
Jose <malun224@gmail.com>
2018-12-06 14:21:26 +0000

    Cliente
    metedos adicionados

--- UberSimples/src/ubersimples/Cliente.java
+++ UberSimples/src/ubersimples/Cliente.java
@@ -5,18 +5,23 @@
  */
 package ubersimples;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Crisanto
  */
 public abstract class Cliente {
-    
-    String nome;
-    String password;
-    
-    
+
+    protected String username;
+    protected String password;
+    protected int userStatus; // 0 o user não está logado / 1 o user esta logado
+
     // Cliente User
-    
     // Registar;
     // Logar;
     // Inserir origem da viagem;
@@ -25,24 +30,20 @@ public abstract class Cliente {
     // Atribuir uma pontuação (1 a 5) ao condutor para uma viagem específica;
     // Visualizar o seu histórico de viagens e respetiva pontuação atribuída;
     // Sair.
-    
     // Cliente Condutor
-    
     // Registar;
     // Logar;
     // Alterar estado (disponível / indisponível)
     // Receber pedidos de viatura com condutor para uma viagem específica e aceitar ou rejeitar os mesmos;
     // Visualizar o seu histórico de viagens e respetiva pontuação recebida;
     // Sair.
-
     // o que têm em comum
-
-    public String getNome() {
-        return nome;
+    public String getUsername() {
+        return username;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -53,8 +54,69 @@ public abstract class Cliente {
         this.password = password;
     }
 
-    
-    
+    public int enterCredentials() {
+        int re = 0;
+
+        String ClientUserName;
+        String ClientPassword;
+
+        BufferedReader clientRead = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter your username: ");
+        try {
+            ClientUserName = clientRead.readLine();
+            System.out.print("Enter your password: ");
+            try {
+                ClientPassword = clientRead.readLine();
+                this.username = ClientUserName;
+                this.password = ClientPassword;
+                re = 1;
+            } catch (IOException ex) {
+                Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return re; // se re for 1 as credenciais foram inseridas corretamente
+    }
+
+    public int Registo() {
+        int re = 0;
+
+        // le credenciais
+        if(enterCredentials() == 1){ // faz o resto
+            // mandar para o server
+            // se retornar 0 é porque já existe o user
+            // se retornar 1 é porque foi registado com sucesso
+        }else{
+            System.out.println("erro ao inserir credenciais");
+        }
+        
+        return re;
+    }
+
+    public int LogIn() {
+        int re = 0;
+        
+        // le credenciais
+        if(enterCredentials() == 1){ // faz o resto
+            
+        }else{
+            // mandar para o server
+            // se retornar 0 é porque o user não existe
+            // se retornar 1 é porque a password está errada
+            // se retornar 2 é porque o logIn foi feito com sucesso
+        }
+        
+        return re;
+    }
+
+    public int menu() {
+        int re = 0;
+        
+        return re;
+    }
+
     abstract void historico();
-    
+
 }
