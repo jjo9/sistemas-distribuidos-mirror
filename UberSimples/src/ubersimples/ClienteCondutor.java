0ae8b33a621011405ac77c7a8d877eec50efc547
Jose <malun224@gmail.com>
2018-12-07 00:21:20 +0000

    clienteComunications
    criação de envio e receção de mensagens dos clienstes, planeamento

--- UberSimples/src/ubersimples/ClienteCondutor.java
+++ UberSimples/src/ubersimples/ClienteCondutor.java
@@ -47,6 +47,8 @@ public class ClienteCondutor extends Cliente {
         // para ver o historico fazer pedido ao servidor, este envia toda a informação
     }
     
+    public void 
+    
     public int mudarEstado() {
         int re = 0;
         
