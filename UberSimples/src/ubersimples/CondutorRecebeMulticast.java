eed48313dfcd3848767370a7e52e1f56cba47543
Jose <malun224@gmail.com>
2018-12-22 19:19:18 +0000

    entrada de Multicast
    já processa a informação, o Add e o Drop j á funciona

--- UberSimples/src/ubersimples/CondutorRecebeMulticast.java
+++ UberSimples/src/ubersimples/CondutorRecebeMulticast.java
@@ -59,15 +59,29 @@ public class CondutorRecebeMulticast extends Thread {
             }
 
             String received = new String(packet.getData(), 0, packet.getLength());
-            this.mensagensMulticast.add(received); // preciso de fazer isto sequer ? tipo para que é que estou a guardar ?
+            this.mensagensMulticast.add(received); // preciso de fazer isto sequer ? tipo para que é que estou a guardar ? por isso também não preciso de tirar no fim
             System.out.println("recebe: " + received);
-            
-            
-            // fazer alguma coisa para retirar da lista
+
+            // fazer alguma coisa para retirar da lista de pedidos de mensagens
             // fazer split "/"
-            // se for "Add/"  // foi este o nome que dei ?!!?!?
-            // se for "Drop/" -> ir a "this.listaDePedidos" e retirar esse pedido
-            
+            String[] receivedTemp = received.split("/");
+
+            if (receivedTemp[0].equals("Add")) {// se for "Add/"  // foi este o nome que dei ?!!?!?
+                this.listaDePedidos.add(receivedTemp[1] + "/" + receivedTemp[2] + "/" + receivedTemp[3]); // "User/Origem/Destino"
+            } else if (receivedTemp[0].equals("Drop")) {// se for "Drop/" -> ir a "this.listaDePedidos" e retirar esse pedido
+
+                for (int x = 0; x < this.listaDePedidos.getSize(); x++) { // encontrar o pedido que será removido
+                    if (this.listaDePedidos.get().get(x).equals(receivedTemp[1] + "/" + receivedTemp[2] + "/" + receivedTemp[3])) { // se for igual ao pedido de viagem que chegou para ser removido
+                        this.listaDePedidos.removeFromPosition(x); // remove pedido
+                        break;
+                    }
+                }
+                System.out.println("Removido com sucesso");
+
+            } else {
+                System.out.println("Erro");
+            }
+
         }
 
     }
